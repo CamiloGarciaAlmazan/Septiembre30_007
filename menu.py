7f17ee1b53f3b6bfcd4b1ec94c05fea558eed985
unknown <cam.garciaa@alumnos.duoc.cl>
2020-10-06 12:05:44 -0300

    se agrego metodo contar persona  del menu osea el 2

--- menu.py
+++ menu.py
@@ -19,12 +19,33 @@ def Numeros():
     print("Cantidad de números positivos: " + str(pos)+
     "\n Cantidad de números negativos : "+ str(neg)+ 
     "\n Cantidad de números iguales a cero: " + str(cero))    
-
+         
     pausa=input("presione una tecla para continuar")
 
 
 def Datos():
     print("**** opcion 2 de datos de personas****")
+    #ingresar para n personas donde n es un numero ingresado
+    #por teclado el nombre y edad
+    # cantidad de mayores de edad y cantidad de menores de edad
+    # luego subir la modificacion a github
+    # "se programo la opcion 2 del menu"
+    nombre="carlos"
+    edad=0
+    mayoresEdad=0
+    menoresEdad=0
+    veces= int(input("Cuantas numero de personas desea ingresar?: ")) 
+    for x in range(veces): 
+        nombre=str(input("Ingrese un nombre: "))
+        edad=int(input("Ingrese un edad: "))
+        if (edad>=18):
+            mayoresEdad=mayoresEdad+1
+        elif(edad<18):
+            menoresEdad=menoresEdad+1
+
+    print("Cantidad de personas mayores de edad: " + str(mayoresEdad)+
+    "\n Cantidad de personas menores de edad : "+ str(menoresEdad))
+     
     pausa=input("presione una tecla para continuar")
 
 
